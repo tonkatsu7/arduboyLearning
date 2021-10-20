e85cd09fefa5935acac28a8a787c65efbcc82a9c
tonka7su <tonka7su@gmail.com>
2021-10-20 16:50:40 +1100

    implemented scoring

--- 7_makePongFromScratch/sketch_oct16a/sketch_oct16a.ino
+++ 7_makePongFromScratch/sketch_oct16a/sketch_oct16a.ino
@@ -5,18 +5,25 @@
 #include <Arduboy2.h>
 Arduboy2 arduboy;
 
+// overall state of the program
 int gamestate = 0;
+// ball
 int ballx = 62;
 int bally = 0;
 int ballsize = 4;
 int ballright = 1;
 int balldown = 1;
+// player paddle
 int paddlewidth = 4;
 int paddleheight = 9;
 int playerx = 0;
 int playery = 0;
+// computer paddle
 int computerx = 127 - paddlewidth;
 int computery = 0;
+// scoring
+int playerscore = 0;
+int computerscore = 0;
 
 void setup() {
   arduboy.begin();
@@ -45,28 +52,13 @@ void loop() {
   
   case 1:
     // gameplay screen
-    arduboy.setCursor(0, 0);
-    arduboy.print("Gameplay");
+    arduboy.setCursor(20, 0);
+    arduboy.print(playerscore);
 
-    // draw player paddle
-    arduboy.fillRect(playerx, playery, paddlewidth, paddleheight, WHITE);
+    arduboy.setCursor(101, 0);
+    arduboy.print(computerscore);
 
-    if (arduboy.pressed(UP_BUTTON) && playery > 0) {
-      playery = playery - 1;
-    }
-    if (arduboy.pressed(DOWN_BUTTON) && playery + paddleheight < 63) {
-      playery = playery + 1;
-    }
-
-    // draw computer paddle
-    arduboy.fillRect(computerx, computery, paddlewidth, paddleheight, WHITE);
-
-    if (bally < computery) {
-      computery = computery - 1;
-    }
-    if (bally + ballsize > computery + paddleheight) {
-      computery = computery + 1;
-    }
+    // ##### DRAW BALL #####
 
     // draw ball
     arduboy.fillRect(ballx, bally, ballsize, ballsize, WHITE);
@@ -100,6 +92,57 @@ void loop() {
       balldown = -1;
     }
 
+    // ##### DRAW PLAYER PADDLE #####
+
+    // draw player paddle
+    arduboy.fillRect(playerx, playery, paddlewidth, paddleheight, WHITE);
+
+    if (arduboy.pressed(UP_BUTTON) && playery > 0) {
+      playery = playery - 1;
+    }
+    if (arduboy.pressed(DOWN_BUTTON) && playery + paddleheight < 63) {
+      playery = playery + 1;
+    }
+    
+    // ##### DRAW COMPUTER PADDLE #####
+
+    // draw computer paddle
+    arduboy.fillRect(computerx, computery, paddlewidth, paddleheight, WHITE);
+
+    if (ballx > 115 | random(0, 20) == 1) {
+      if (bally < computery) {
+        computery = computery - 1;
+      }
+      if (bally + ballsize > computery + paddleheight) {
+        computery = computery + 1;
+      }  
+    }
+
+    // ##### SCORING #####
+
+    // player scores a point when the ball goes off the screen on the right
+    if (ballx < -10) {
+      ballx = 63;
+      computerscore = computerscore + 1;
+    }
+
+    // computer scores a point when the ball goes off the screen on the left
+    if (ballx > 130) {
+      ballx = 63;
+      playerscore = playerscore + 1;
+    }
+
+    // player wins
+    if (playerscore == 5) {
+      gamestate = 2;
+    }
+
+    // player loses
+    if (computerscore == 5) {
+      gamestate = 3;
+    }
+
+    // player concede?
     if (arduboy.justPressed(A_BUTTON)) {
       gamestate = 2;
     }
@@ -109,6 +152,7 @@ void loop() {
     // win screen
     arduboy.setCursor(0, 0);
     arduboy.print("Win Screen");
+    reset();
     if (arduboy.justPressed(A_BUTTON)) {
       gamestate = 3;
     }
@@ -118,6 +162,7 @@ void loop() {
     // game over screen
     arduboy.setCursor(0, 0);
     arduboy.print("Game over screen");
+    reset();
     if (arduboy.justPressed(A_BUTTON)) {
       gamestate = 0;
     }
@@ -125,4 +170,10 @@ void loop() {
   }
 
   arduboy.display();
+}
+
+void reset() {
+  ballx = 0;
+  playerscore = 0;
+  computerscore = 0;
 }
