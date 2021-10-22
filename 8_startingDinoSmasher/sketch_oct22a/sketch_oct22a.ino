0a8c1978fdd1bcdec8d5bb74b7a2ff1c8b677806
tonka7su <tonka7su@gmail.com>
2021-10-22 17:44:46 +1100

    cpmpleted part 9.6 - bounding the map

--- 8_startingDinoSmasher/sketch_oct22a/sketch_oct22a.ino
+++ 8_startingDinoSmasher/sketch_oct22a/sketch_oct22a.ino
@@ -16,25 +16,27 @@
 #define WATER       1
 #define TREES       2
 #define STONE       3
+#define PLAYER_SIZE       16
+#define PLAYER_X_OFFSET   WIDTH / 2 - PLAYER_SIZE / 2
+#define PLAYER_Y_OFFSET   HEIGHT / 2 - PLAYER_SIZE / 2
 
 const unsigned char tiles[] PROGMEM = {
 // width, height,
 16, 16,
 
-// grass
+//Grass
 0xff, 0x7f, 0xfb, 0xff, 0xff, 0xbf, 0xff, 0xff, 0xf7, 0xff, 0xfd, 0xff, 0xff, 0xf7, 0x7f, 0xff, 
 0xdf, 0xff, 0xff, 0xfb, 0x7f, 0xff, 0xff, 0xff, 0xef, 0xfe, 0xff, 0xff, 0xfb, 0xff, 0x7f, 0xff, 
 
-// water
+//Water
 0x08, 0x10, 0x10, 0x08, 0x10, 0x08, 0x10, 0x10, 0x10, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
 0x20, 0x40, 0x40, 0x20, 0x00, 0x01, 0x02, 0x02, 0x01, 0x02, 0x02, 0x01, 0x02, 0x21, 0x40, 0x40, 
 
-
-// tre
+//Tree
 0xff, 0x1f, 0x5b, 0x3f, 0xeb, 0xdd, 0xff, 0xf7, 0xbb, 0xef, 0xfd, 0x7f, 0xe3, 0xcb, 0xe3, 0xff, 
 0xff, 0xc7, 0x96, 0xc7, 0xff, 0xff, 0xef, 0xfd, 0xff, 0xe3, 0xcb, 0xe3, 0xff, 0xff, 0x7b, 0xff, 
 
-// stone
+//Stone
 0xff, 0xdf, 0x7b, 0x3f, 0x9f, 0x6f, 0x77, 0xab, 0xdb, 0xd7, 0xcd, 0x5f, 0xbf, 0x77, 0xff, 0xff, 
 0xff, 0xc1, 0xdc, 0xd3, 0xaf, 0x9f, 0xae, 0xb0, 0xbb, 0xbd, 0xbd, 0xba, 0xd7, 0xcc, 0x63, 0xff, 
 };
@@ -69,6 +71,11 @@ void move_up() {
 void move_down() {
   player_y += SPEED;
 }
+
+void drawPlayer() {
+  arduboy.fillRect(PLAYER_X_OFFSET, PLAYER_Y_OFFSET, PLAYER_SIZE, PLAYER_SIZE, BLACK);
+}
+
 void drawWorld() {
   const int tilesWide = WIDTH / TILE_SIZE + 1;
   const int tilesTall = HEIGHT / TILE_SIZE + 1;
@@ -77,32 +84,42 @@ void drawWorld() {
     for (int i = 0; i < tilesWide; i++) {
       const int tilex = i - mapx / TILE_SIZE;
       const int tiley = j - mapy / TILE_SIZE;
-      Sprites::drawOverwrite(i * TILE_SIZE + mapx % TILE_SIZE, j * TILE_SIZE + mapy % TILE_SIZE, tiles, world[tilex][tiley]);
+      if (tilex >= 0 && tiley >= 0 && tilex < WORLD_WIDTH && tiley < WORLD_HEIGHT) {
+        Sprites::drawOverwrite(i * TILE_SIZE + mapx % TILE_SIZE, j * TILE_SIZE + mapy % TILE_SIZE, tiles, world[tiley][tilex]);
+      }
     }
-    arduboy.print("\n");
+    // arduboy.print("\n");
   }
-
-  arduboy.fillRect(WIDTH / 2 - 8, HEIGHT / 2 - 8, 16, 16, BLACK);
 
   arduboy.fillRect(0, 0, 48, 8, BLACK);
   arduboy.setCursor(0, 0);
-  arduboy.print(0 - mapx / TILE_SIZE);
+  // arduboy.print(0 - mapx / TILE_SIZE);
+  arduboy.print(mapx);
   arduboy.print(",");
-  arduboy.print(0 - mapy / TILE_SIZE);
+  // arduboy.print(0 - mapy / TILE_SIZE);
+  arduboy.print(mapy);
 }
 
 void playerInput() {
   if (arduboy.pressed(UP_BUTTON)) {
-    mapy += 1;
+    if (mapy < PLAYER_Y_OFFSET) {
+      mapy += 1;
+    }
   }
   if (arduboy.pressed(DOWN_BUTTON)) {
-    mapy -= 1;
+    if (PLAYER_Y_OFFSET + PLAYER_SIZE < mapy + WORLD_HEIGHT * TILE_SIZE) {
+      mapy -= 1;
+    }
   }
   if (arduboy.pressed(LEFT_BUTTON)) {
-    mapx += 1;
+    if (mapx < PLAYER_X_OFFSET) {
+      mapx += 1;
+    }
   }
   if (arduboy.pressed(RIGHT_BUTTON)) {
-    mapx -= 1;
+    if (PLAYER_X_OFFSET + PLAYER_SIZE < mapx + WORLD_WIDTH * TILE_SIZE) {
+      mapx -= 1;
+    }
   }
 }
 
@@ -117,6 +134,7 @@ void titleScreen() {
 void gameplay() {
   playerInput();
   drawWorld();
+  drawPlayer();
 
   if (arduboy.justPressed(A_BUTTON)) {
     gamestate = GAME_OVER;
